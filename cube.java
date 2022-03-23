3de15e97332c4d40eaf5207974bf42efb8837bd6
Jithukrish <94125038+Jithukrish@users.noreply.github.com>
2022-03-23 14:41:45 +0530

    Add files via upload

---
+++ cube.java
@@ -0,0 +1,26 @@
+//cube of number
+import java.util.Scanner;
+public class cube{
+
+    
+  public static void main(String[] args)
+    {   
+      int i,n;
+           Scanner in = new Scanner(System.in);
+           System.out.println("Input value: ");
+           n =in.nextInt(); 
+          for(i=1;i<n;i++)
+  {
+   System.out.println("number is : "+i+" and cube of:  "+i+" is :"  +(i*i*i));
+   }
+  }
+ }
+/** out put
+Input value: 
+5
+number is : 1 and cube of:  1 is :1
+number is : 2 and cube of:  2 is :8
+number is : 3 and cube of:  3 is :27
+number is : 4 and cube of:  4 is :64
+*/
+
---
+++ inc.java
@@ -0,0 +1,57 @@
+//write a program that accept 3 numbers check increasing,decreasing or both
+import java.util.Scanner;
+public class inc{
+
+    
+  public static void main(String[] args)
+    {   
+      
+           Scanner in = new Scanner(System.in);
+           System.out.println("first number: ");
+           double x=in.nextDouble(); 
+
+           System.out.println("2nd number : ");
+           double y=in.nextDouble();
+
+            System.out.println("3rd number: ");
+           double z=in.nextDouble();
+
+     if(x<y&&y<z)
+  {
+    System.out.println("increasing order ");
+  }
+  else if(x>y&&y>z)
+  {  
+  System.out.println("decreasing order ");
+  }
+  else
+ {
+ System.out.println(" increasing or decreasing ");
+  }
+}
+}
+/** out put
+ first number: 
+5
+2nd number : 
+6
+3rd number: 
+7
+increasing order
+
+first number: 
+5
+2nd number : 
+4
+3rd number: 
+3
+decreasing order
+
+first number: 
+5
+2nd number : 
+6
+3rd number: 
+1
+ increasing and decreasing 
+*/
