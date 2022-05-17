b1880d82a265b694d2e7674d775098d09c5ca4dc
Jithukrish <94125038+Jithukrish@users.noreply.github.com>
2022-05-17 15:37:07 +0530

    Delete Rectangle.java

--- Graphics/Rectangle.java
+++
@@ -1,9 +0,0 @@
-package Graphics;
-interface AreaR{
-void area(int l,int b);
-}
-public class Rectangle implements AreaR{
-    public void area(int l,int b){
-     System.out.println("area of rectangle : "+(l*b)+"------");
-}
-}
