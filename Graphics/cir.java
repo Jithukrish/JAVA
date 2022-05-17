071abf828b537dcbd482d2889b68f2a47ac90b09
Jithukrish <94125038+Jithukrish@users.noreply.github.com>
2022-05-17 15:36:29 +0530

    Delete cir.java

--- Graphics/cir.java
+++
@@ -1,9 +0,0 @@
-package Graphics;
-interface areac{
-void area(double r);
-}
-public class cir implements areac{
-    public void area(double r){
-     System.out.println("area of circle : "+(3.14*r*r)+"------");
-}
-}
