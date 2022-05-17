cc8c7b1dc53b5bcfee7b16c165c95a47b8fe5dac
Jithukrish <94125038+Jithukrish@users.noreply.github.com>
2022-05-17 15:37:26 +0530

    Delete Sq.java

--- Graphics/Sq.java
+++
@@ -1,9 +0,0 @@
-package Graphics;
-interface AreaSQ{
-void area(int a);
-}
-public class Sq implements AreaSQ{
-    public void area(int a){
-     System.out.println("area of sq : "+(a*a)+"------");
-}
-}
