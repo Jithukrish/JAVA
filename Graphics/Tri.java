f553cc0b71dbafe8e0b036dd4a4673f3ae6e0039
Jithukrish <94125038+Jithukrish@users.noreply.github.com>
2022-05-17 15:37:34 +0530

    Delete Tri.java

--- Graphics/Tri.java
+++
@@ -1,9 +0,0 @@
-package Graphics;
-interface AreaT{
-void area(int h,int b);
-}
-public class Tri implements AreaT{
-    public void area(int h,int b){
-     System.out.println("area of triangle : "+(b*h*0.5)+"------");
-}
-}
